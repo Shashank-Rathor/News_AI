c0275fba2420775adc085b64119825526dcdb808
Shashank Rathor <shashankrathor180@gmail.com>
2020-09-02 18:47:05 +0530

    Updates

---
+++ precache-manifest.aee11f86d4ba66b95802a29867ec94f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e616d0575da04709fdd40ca2290e5f",
+    "url": "/News_AI/index.html"
+  },
+  {
+    "revision": "6401f5d5f25940856040",
+    "url": "/News_AI/static/css/main.eea4a857.chunk.css"
+  },
+  {
+    "revision": "8ed50b21b523582e3efb",
+    "url": "/News_AI/static/js/2.db04eb51.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/News_AI/static/js/2.db04eb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6401f5d5f25940856040",
+    "url": "/News_AI/static/js/main.378a736e.chunk.js"
+  },
+  {
+    "revision": "4b10bdabb2ee0b2240f6",
+    "url": "/News_AI/static/js/runtime-main.0eb7cff5.js"
+  },
+  {
+    "revision": "8011bbe08d2cf7a6b5e8e75b72d1da18",
+    "url": "/News_AI/static/media/background.8011bbe0.jpg"
+  }
+]);
